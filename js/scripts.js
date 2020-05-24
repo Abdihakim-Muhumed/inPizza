7e0e122afb94819006ee1d1a34bfa2e5bd28af12
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-24 19:18:14 +0300

    Edit back-end

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//back-end
+function Order(size,crust,toppings,numberofPizza,type,location,totalCost){
+    this.size=size;
+    this.crust=crust;
+    this.toppings=toppings;
+    this.pizzas=numberofPizza;
+    this.type= type;
+    this.location=location;
+    this.totalCost=totalCost;
+}
