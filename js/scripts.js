2fab960b83e42c5759ba9714fb5017af1ac8415a
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-25 18:55:25 +0300

    scripts.js//front-end edited

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,121 @@
 //back-end
-function Order(size,flavour,crusts,toppings,numberOfPizza,type,location,totalCost){
-    this.size=size;
-    this.flavour=flavour;
-    this.crust=crusts;
-    this.toppings=toppings;
+function Order(numberOfPizza,type,location,totalCost){
     this.pizzas=numberOfPizza;
     this.type= type;
     this.location=location;
     this.totalCost=totalCost;
 }
+function Pizza(size,flavour,crusts,toppings){
+    this.size=size;
+    this.flavour=flavour;
+    this.crust=crusts;
+    this.toppings=toppings;
+   
+}
 
 //front-end
-$(document).ready(){
-    $("form.order-form").submit(function(){
+$(document).ready(function(){
+    alert("yow1");
+    $("#order").click(function(){
         var size=$("input[name='size']:checked").val();
         var flavour = $("input[name='flavour']:checked").val();
         var crusts = $("input[name='crusts']:checked").val();
         var toppings = $("input[class='toppings']:checked").val();
-        var numberOfPizza = $("#number-pizza").val();
-        var type = $("input[name='type']:checked").val();
-        var location = $("#location").val();
+        var initialCost,toppingsCost,crustCost,newPizza;      
+       
         if($("input[name='size']:checked").val()==="Regular"){
-            var initialCost = 500;
+             initialCost = 500;
             if($("input[class='toppings']:checked").val().length >0){
-                var toppingsCost = 50;
-                
+                 toppingsCost = 50;               
             }
             else{
                 toppingsCost=0;
             }
+            if($("input[class='classic']:checked").val().length >0){
+                 crustCost=200;
+            }  
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=300;
+            }
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=400;
+            }
+           
         }
         else if($("input[name='size']:checked").val()==="Medium"){
-            var initialCost = 700;
+             initialCost = 700;
             if($("input[class='toppings']:checked").val().length >0){
-                var toppingsCost = 100;
+                 toppingsCost = 100;
                 
             }
             else{
                 toppingsCost=0;
+            }
+            if($("input[class='classic']:checked").val().length >0){
+                 crustCost=200;
+            }  
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=300;
+            }
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=400;
             }
         }
         else if($("input[name='size']:checked").val()==="Large"){
-            var initialCost = 900;
+            initialCost = 900;
             if($("input[class='toppings']:checked").val().length >0){
-                var toppingsCost = 150;
+                 toppingsCost = 150;
                 
             }
             else{
                 toppingsCost=0;
+            }
+            if($("input[class='classic']:checked").val().length >0){
+                 crustCost=200;
+            }  
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=300;
+            }
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=400;
             }
         }
         else if($("input[name='size']:checked").val()==="Mega"){
-            var initialCost = 1100;
+             initialCost = 1100;
             if($("input[class='toppings']:checked").val().length >0){
-                var toppingsCost = 200;
+                toppingsCost = 200;
                 
             }
             else{
                 toppingsCost=0;
             }
+            if($("input[class='classic']:checked").val().length >0){
+                 crustCost=200;
+            }  
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=300;
+            }
+            else if($("input[class='median']:checked").val().length >0){
+                 crustCost=400;
+            }
         }
+        var price = initialCost+crustCost+toppingsCost;
         
-        if($("input[name='type']:checked").val()=="Deliver"){
-            var deliveryCost=100;
-        }
-        else{
-            deliveryCost=0;
-        }
-        var totalCost = initialCost+toppingsCost+deliveryCost;
+         newPizza = new Pizza(size,flavour,crusts,toppings);
+        $(".order-form").show();
+        alert(newPizza);
+        $("#order-size").append(newPizza.size);
+        $("#flavour").append(newPizza.flavour);
+        $("#crust").append(newPizza.crusts);
+        $("#initial-cost").append(initialCost);
+        $("crust-cost").append(crustCost);
+        $("#toppings").append(newPizza.toppings);
+        $("#toppings-cost").append(toppingsCost);
+        $("#pizza-cost").append(price);
 
-        var newOrder = new Order(size,flavour,crusts,toppings,numberOfPizza,type,locstion,totalCost);
-        
+        $("#deliver").click(function(){
+            $(".location").show();
+        });
     });
-}
+    
+});
